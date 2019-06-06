7630220062fa3301b6089b3ffe4134d78e6b8a5f
Z <kerenyi.nagy.zoltan@gmail.com>
2019-06-06 23:57:11 +0200

    Create arranger_1.cs

---
+++ arranger_1.cs
@@ -0,0 +1,12 @@
+for (int i = 0; i < array.Length -1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
