8b8008c38e1577b7a784eeb1714b4182f507b35c
Z <kerenyi.nagy.zoltan@gmail.com>
2019-06-23 08:07:26 +0200

    Update BubbleSorting.cs

--- BubbleSorting.cs
+++ BubbleSorting.cs
@@ -7,8 +7,6 @@ Then we switch elements in the inner loop if the first is bigger than the second
 
 */
 
-
-
 for (int i = numbers.Length; i > 0; i--)
 {           
             for (int j = 0; j < i - 1  ; j++)
@@ -18,7 +16,6 @@ for (int i = numbers.Length; i > 0; i--)
                         int temp = numbers[j];
                         numbers[j] = numbers[j + 1];
                         numbers[j + 1] = temp;
-
                     }
              }
 }
