2dbcdd6258f65c2d4e99973251614071f64ba414
Z <kerenyi.nagy.zoltan@gmail.com>
2019-06-23 08:45:12 +0200

    Update linear_and_binary_search.cs

--- linear_and_binary_search.cs
+++ linear_and_binary_search.cs
@@ -1,49 +1,70 @@
-class Program {
-        static void Main( string[] args ) {
-            Console.WriteLine("Generating the list...");
-            int[] numbers = new int[Int32.MaxValue / 10];
+using System;
+using System.Diagnostics;
 
-            for ( int i = 0; i < numbers.Length; i++ ) {
+namespace Linear_vs_Binary_Search {
+    class Program {
+
+        static int BinarySearch(int[] array, int key) {
+            int low = 0;
+            int high = array.Length;
+            int middle;
+
+            while (low <= high)
+            {
+                middle = (low + high) / 2;
+
+                if (middle == key)
+                {
+                    return array[middle];
+                }
+                else if (middle < key)
+                {
+                    low = middle + 1;
+                }
+                else
+                    high = middle - 1;
+            }
+            return 0;
+        }
+
+        static int LinearSearch(int[] array, int key) {
+
+            int i = 0;
+            while (i < array.Length && array[i] != key)
+            {
+                i++;
+            }
+            if (i < array.Length)
+            {
+                return i;
+            }
+            return 0;
+        }
+
+
+        static void Main(string[] args) {
+
+            int[] numbers = new int[10000];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
                 numbers[i] = i;
             }
 
-            Console.WriteLine("Which number to look for betveen 0 - {0}", numbers.Length);
-            int item = Convert.ToInt32(Console.ReadLine());
-
             Stopwatch stopwatch = new Stopwatch();
+
+
             stopwatch.Start();
-            // linear search
-            for ( int i = 0; i < numbers.Length; i++ ) {
-                if ( numbers[i] == item ) {
-                    break;
-                }
-            }
+            LinearSearch(numbers, 9999);
             stopwatch.Stop();
-            Console.Write("Linear search ends");
-            Console.WriteLine(stopwatch.Elapsed);
+            Console.WriteLine("Elapsed time after Linear Search: {0}", stopwatch.Elapsed);
+
             stopwatch.Restart();
-
-            // binary search
-
-            int left = 0;
-            int right = numbers.Length - 1;
-
-            while ( left <= right ) {
-                int middle = (left + right) / 2;
-
-                if ( numbers[middle] == item )
-                    break;
-                if ( item < numbers[middle] )
-                    right = middle - 1;
-                else
-                    left = middle + 1;
-            }
+            BinarySearch(numbers, 9999);
             stopwatch.Stop();
-            Console.Write("Binary search ends");
-            Console.WriteLine(stopwatch.Elapsed);
-            
-            Console.ReadKey();
+            Console.WriteLine("Elapsed time after Binary Search: {0}", stopwatch.Elapsed);
 
-            
+            Console.ReadKey();
         }
     }
+}
