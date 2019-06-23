ccb05e65fbe2bddcab657fab78808bb94ce39548
Z <kerenyi.nagy.zoltan@gmail.com>
2019-06-23 19:40:15 +0200

    Update BinarySearch.cs

--- BinarySearch.cs
+++ BinarySearch.cs
@@ -6,23 +6,22 @@ Binary or Logarithmic Search
 
 static int BinarySearch(int[] array, int key) {
             int low = 0;
-            int high = array.Length - 1;
+            int high = array.Length;
             int middle;
 
-            while (low <= high)
-
+            do
             {
                 middle = (low + high) / 2;
 
-                if (array[middle] == key)
-                {
-                    return middle;
-                }
-                else if (array[middle] < key)
+                if (array[middle] < key)
                 {
                     low = middle + 1;
                 }
-                else
+                else if (array[middle] > key)
+                {
                     high = middle - 1;
-            }
-            return 0;
+                }
+            } while (low <= high && array[middle] != key);
+
+            return middle;
+        }
